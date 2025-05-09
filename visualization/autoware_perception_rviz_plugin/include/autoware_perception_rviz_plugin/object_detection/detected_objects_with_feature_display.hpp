4a65dafec7728209dc4015c513920215d259ddae
lei.gu <lei.gu@tier4.jp>
2025-05-09 15:46:07 +0900

    Squashed commit of the following:
    commit 699e657c3997e0c3457d9c1f5fffe1081c4433cc
    Merge: 4833afd811 e876ece2f8
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Fri May 9 11:14:48 2025 +0900
    
        Merge branch 'main' into feat/autoware_perception_rviz_plugin/detected_objects_with_feature_display
    
    commit 4833afd8114364625a4a9a82b237e72a09c737be
    Author: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Date:   Fri May 9 02:13:09 2025 +0000
    
        style(pre-commit): autofix
    
    commit d7bf97d85c1c97300adf52b7ace62a7c08b78402
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Fri May 9 10:53:33 2025 +0900
    
        fix all problems of rviz
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit 91ec2882a505df6996d49a2395f977eae1841314
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Thu May 8 19:22:56 2025 +0900
    
        format fix
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit fa1e680ab138253831398c51c415dd3861ea298b
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Thu May 8 17:56:16 2025 +0900
    
        helper to better structure
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit 2e4ba008e8c3351fc12f33b79f2fe41c492b1f3c
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Thu May 8 16:26:16 2025 +0900
    
        colorbar visualization optimized
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit 25e4b9f4131cf38ce89c9b8b28dee0ed6562a4c3
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Thu May 8 15:42:48 2025 +0900
    
        basic functions all implemented
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit 3e3db86a1f3ff266cb00b8b83fa57231ee8e2fb8
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Thu May 8 10:31:02 2025 +0900
    
        colorbar
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit a6be3ce4a2a3fc48b54ba798af7875d6b071d88b
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Wed May 7 18:05:45 2025 +0900
    
        colormap fully implemented
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit 46762b344541580d3411f61ea78828e5f35d9cfb
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Wed May 7 17:49:07 2025 +0900
    
        colormap implemented
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
    commit e3024f1d2865ca76c0b8e338fa5c2d6bd282dd22
    Author: lei.gu <lei.gu@tier4.jp>
    Date:   Thu Apr 17 10:30:24 2025 +0900
    
        feat(euclidean_cluster): add markers for clusters
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
        remove filter
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
        profiling
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
        rviz detected_objects_with_feature
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
        detected objects
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
        stage all commits
    
        Signed-off-by: lei.gu <lei.gu@tier4.jp>
    
        Revert non-visualization changes to state of 43480ef7
    
    Signed-off-by: lei.gu <lei.gu@tier4.jp>

--- visualization/autoware_perception_rviz_plugin/include/autoware_perception_rviz_plugin/object_detection/detected_objects_with_feature_display.hpp
+++ visualization/autoware_perception_rviz_plugin/include/autoware_perception_rviz_plugin/object_detection/detected_objects_with_feature_display.hpp
@@ -15,22 +15,28 @@
 #define AUTOWARE_PERCEPTION_RVIZ_PLUGIN__OBJECT_DETECTION__DETECTED_OBJECTS_WITH_FEATURE_DISPLAY_HPP_
 
 #include "autoware_perception_rviz_plugin/object_detection/object_polygon_display_base.hpp"
+#include "detected_objects_with_feature_helper.hpp"
 
+#include <QImage>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QWidget>
 #include <rviz_common/properties/bool_property.hpp>
 #include <rviz_common/properties/float_property.hpp>
 #include <rviz_common/ros_topic_display.hpp>
 
-#include <tf2_ros/transform_listener.h>
-#include <tf2_ros/buffer.h>
 #include <tier4_perception_msgs/msg/detail/detected_objects_with_feature__struct.hpp>
 #include <tier4_perception_msgs/msg/detected_objects_with_feature.hpp>
 
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 namespace autoware
 {
 namespace rviz_plugins
 {
 namespace object_detection
 {
+
 /// \brief Class defining rviz plugin to visualize DetectedObjects
 class AUTOWARE_PERCEPTION_RVIZ_PLUGIN_PUBLIC DetectedObjectsWithFeatureDisplay
 : public rviz_common::RosTopicDisplay<tier4_perception_msgs::msg::DetectedObjectsWithFeature>
@@ -45,11 +51,13 @@ public:
   using DetectedObjectsWithFeature = tier4_perception_msgs::msg::DetectedObjectsWithFeature;
   using RosTopicDisplay = rviz_common::RosTopicDisplay<DetectedObjectsWithFeature>;
 
+  DetectedObjectsWithFeatureDisplay(
+    const std::string & default_topic = "detected_objects_with_feature");
+  ~DetectedObjectsWithFeatureDisplay() override;
 
-  DetectedObjectsWithFeatureDisplay(const std::string & default_topic = "detected_objects_with_feature");
 protected:
   void onInitialize() override;
-  void reset() override;  
+  void reset() override;
   void processMessage(DetectedObjectsWithFeature::ConstSharedPtr msg) override;
 
   void load(const rviz_common::Config & config) override
@@ -57,27 +65,34 @@ protected:
     RosTopicDisplay::Display::load(config);
     m_marker_common.load(config);
   }
-
+  void onPropertyChanged(const rviz_common::properties::Property * property);
   void update(float wall_dt, float ros_dt) override { m_marker_common.update(wall_dt, ros_dt); }
-  double get_line_width() { return m_point_size_property.getFloat(); }
   double get_point_size() { return m_point_size_property.getFloat(); }
   QColor get_point_color() { return m_point_color_property.getColor(); }
-  double get_point_alpha() { return m_point_alpha_property.getFloat(); }
+  // Member variable to store the colorbar image
+  QImage m_colorbar_image;
+  // Method to generate the colorbar image
+  void generateColorbar();
+  void updateColorbarVisibility();
+  void updateColormapAndColorbar();
+  void updateColormapPropertiesVisibility();
+
 private:
   // All rviz plugins should have this. Should be initialized with pointer to this class
-  MarkerCommon m_marker_common;  
-  // Property to set line width of object shape
-  rviz_common::properties::FloatProperty m_line_width_property;
+  MarkerCommon m_marker_common;
   // Property to set point size of cluster point cloud
   rviz_common::properties::FloatProperty m_point_size_property;
+  // Property to set color mode of cluster point cloud
+  rviz_common::properties::EnumProperty m_color_mode_property;
+  // Property to set colormap of cluster point cloud
+  rviz_common::properties::EnumProperty m_colormap_property;
   // Property to set point color of cluster point cloud
   rviz_common::properties::ColorProperty m_point_color_property;
-  // Property to show intensity of cluster point cloud
-  rviz_common::properties::BoolProperty m_display_intensity_property;
-  // Property to set point alpha of cluster point cloud
-  rviz_common::properties::FloatProperty m_point_alpha_property;
   // Property to set intensity color threshold of cluster point cloud
   rviz_common::properties::FloatProperty m_intensity_color_scale_max;
+  // Property to show colorbar
+  rviz_common::properties::BoolProperty m_show_colorbar_property;
+
   // Property to set the default topic name
   std::string m_default_topic;
 
@@ -87,7 +102,7 @@ private:
   {
     m_marker_common.addMessage(marker_ptr);
   }
-
+  ColorbarWidget * m_colorbar_widget;
 };
 
 }  // namespace object_detection
--- visualization/autoware_perception_rviz_plugin/src/object_detection/detected_objects_with_feature_display.cpp
+++ visualization/autoware_perception_rviz_plugin/src/object_detection/detected_objects_with_feature_display.cpp
@@ -16,12 +16,14 @@
 
 #include "autoware_perception_rviz_plugin/object_detection/detected_objects_with_feature_display.hpp"
 
+#include "autoware_perception_rviz_plugin/object_detection/detected_objects_with_feature_helper.hpp"
+
+#include <QObject>
+#include <rclcpp/duration.hpp>
+
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <sensor_msgs/msg/point_field.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
-#include <qcolor.h>
-
-#include <rclcpp/duration.hpp>
 
 #include <memory>
 
@@ -31,28 +33,132 @@ namespace rviz_plugins
 {
 namespace object_detection
 {
+
 DetectedObjectsWithFeatureDisplay::DetectedObjectsWithFeatureDisplay(
   const std::string & default_topic)
 : m_marker_common(this),
-  m_line_width_property{"Line Width", 0.03, "Line width of object-shape", this},
   m_point_size_property{"Point Size", 0.1, "Point size of cluster", this},
+  m_color_mode_property("Color Mode", 0, "Color mode of point cloud", this),
+  m_colormap_property{"Colormap", "Jet", "Colormap to use for intensity coloring", this},
   m_point_color_property{"Point Color", QColor(255, 0, 0), "Point color of object-shape", this},
-  m_display_intensity_property{"Display Intensity", true, "Display intensity of point cloud", this},
   m_intensity_color_scale_max{
     "Intensity Threshold", 50.0, "Intensity threshold of point cloud", this},
+  m_show_colorbar_property{
+    "Show Colorbar", false, "Display the colormap colorbar in a separate window", this},
   m_default_topic{default_topic}
 {
   m_intensity_color_scale_max.setMin(1.0);
   m_intensity_color_scale_max.setMax(255.0);
+
+  m_color_mode_property.addOption("Flat", 0);
+  m_color_mode_property.addOption("Intensity", 1);
+  m_color_mode_property.addOption("Cluster", 2);
+
+  const auto & names = getColormapInfo().names;
+  for (int i = 0; i < NUM_COLORMAPS; ++i) {
+    m_colormap_property.addOption(names[i], i);
+  }
 }
 
 void DetectedObjectsWithFeatureDisplay::onInitialize()
 {
   RosTopicDisplay::onInitialize();
   m_marker_common.initialize(this->context_, this->scene_node_);
-  QString message_type = QString::fromStdString(rosidl_generator_traits::name<DetectedObjectsWithFeature>());
+
+  // Set topic message type and description
+  QString message_type =
+    QString::fromStdString(rosidl_generator_traits::name<DetectedObjectsWithFeature>());
   this->topic_property_->setMessageType(message_type);
-  this->topic_property_->setDescription("Topic to subscribe to.");  
+  this->topic_property_->setDescription("Topic to subscribe to.");
+
+  // Generate the colorbar for the colormap
+  generateColorbar();
+  // Create the colorbar widget
+  m_colorbar_widget = new ColorbarWidget();
+
+  // m_colorbar_widget->setWindowTitle("Colorbar");
+  m_colorbar_widget->setColorbarImage(m_colorbar_image);  // Set the generated colorbar image
+  m_colorbar_widget->setMinMax(0.0f, m_intensity_color_scale_max.getFloat());
+  m_colorbar_widget->setWindowFlags(Qt::Tool);
+  m_colorbar_widget->show();
+
+  QObject::connect(
+    &m_color_mode_property, &rviz_common::properties::Property::changed, this,
+    [this]() { updateColormapPropertiesVisibility(); });
+  QObject::connect(
+    &m_show_colorbar_property, &rviz_common::properties::Property::changed, this,
+    [this]() { updateColorbarVisibility(); });
+  QObject::connect(
+    &m_colormap_property, &rviz_common::properties::Property::changed, this,
+    [this]() { updateColormapAndColorbar(); });
+  QObject::connect(
+    &m_intensity_color_scale_max, &rviz_common::properties::Property::changed, this,
+    [this]() { updateColormapAndColorbar(); });
+  updateColormapPropertiesVisibility();
+}
+
+void DetectedObjectsWithFeatureDisplay::updateColormapAndColorbar()
+{
+  generateColorbar();  // Regenerate the image with the current colormap
+  if (m_colorbar_widget) {
+    m_colorbar_widget->setMinMax(0.0f, m_intensity_color_scale_max.getFloat());
+    m_colorbar_widget->setColorbarImage(m_colorbar_image);
+    if (m_colorbar_widget->isVisible()) {
+      m_colorbar_widget->update();  // Request a repaint of the colorbar widget
+    }
+  }
+}
+
+void DetectedObjectsWithFeatureDisplay::updateColormapPropertiesVisibility()
+{
+  int mode = m_color_mode_property.getOptionInt();
+  bool is_intensity_mode_active = (mode == INTENSITY_COLOR);
+  m_colormap_property.setHidden(!is_intensity_mode_active);
+  m_show_colorbar_property.setHidden(!is_intensity_mode_active);
+  m_intensity_color_scale_max.setHidden(!is_intensity_mode_active);
+  m_point_color_property.setHidden(mode != FLAT_COLOR);
+  updateColorbarVisibility();
+}
+
+void DetectedObjectsWithFeatureDisplay::updateColorbarVisibility()
+{
+  if (!m_colorbar_widget) {
+    return;
+  }
+  bool should_show =
+    m_color_mode_property.getOptionInt() == INTENSITY_COLOR && m_show_colorbar_property.getBool();
+  if (should_show && !m_colorbar_widget->isVisible()) {
+    // Ensure the colorbar image and labels are up-to-date before showing
+    updateColormapAndColorbar();  // This generates the image and sets it
+    // updateIntensityMax();      // This sets the min/max labels
+    m_colorbar_widget->show();
+  } else if (!should_show && m_colorbar_widget->isVisible()) {
+    m_colorbar_widget->hide();
+  }
+}
+
+void DetectedObjectsWithFeatureDisplay::generateColorbar()
+{
+  constexpr int width = 20;
+  constexpr int height = 200;
+  const auto & colormap_info = getColormapInfo();
+  ColormapFuncType colormap_fn = colormap_info.getFunctionSafe(m_colormap_property.getOptionInt());
+  // Create a colorbar image
+  m_colorbar_image = QImage(width, height, QImage::Format_RGB32);
+
+  for (int y = 0; y < height; ++y) {
+    float norm = static_cast<float>(y) / (height - 1);
+    std_msgs::msg::ColorRGBA color = colormap_fn(norm);
+    QRgb rgb = qRgb(color.r * 255, color.g * 255, color.b * 255);
+    for (int x = 0; x < width; ++x) {
+      m_colorbar_image.setPixel(x, y, rgb);
+    }
+  }
+}
+
+DetectedObjectsWithFeatureDisplay::~DetectedObjectsWithFeatureDisplay()
+{
+  delete m_colorbar_widget;
 }
 
 void DetectedObjectsWithFeatureDisplay::reset()
@@ -61,69 +167,76 @@ void DetectedObjectsWithFeatureDisplay::reset()
   m_marker_common.clearMarkers();
 }
 
-void DetectedObjectsWithFeatureDisplay::processMessage(DetectedObjectsWithFeature::ConstSharedPtr msg)
+void DetectedObjectsWithFeatureDisplay::processMessage(
+  DetectedObjectsWithFeature::ConstSharedPtr msg)
 {
   clear_markers();
+
+  const int mode = m_color_mode_property.getOptionInt();
+  const float max_intensity = m_intensity_color_scale_max.getFloat();
+  const ColormapFuncType color_fn =
+    getColormapInfo().getFunctionSafe(m_colormap_property.getOptionInt());
+  const auto default_color = [&]() {
+    std_msgs::msg::ColorRGBA c;
+    c.r = get_point_color().redF();
+    c.g = get_point_color().greenF();
+    c.b = get_point_color().blueF();
+    c.a = 1.0f;
+    return c;
+  }();
+
   int id = 0;
   for (const auto & feature_object : msg->feature_objects) {
     const auto & cluster = feature_object.feature.cluster;
-    if (cluster.width <= 0 || cluster.height <= 0) {      
+    if (cluster.width == 0 || cluster.height == 0) {
       continue;  // Skip this cluster
     }
     // Create a marker to display the cluster point cloud
-    double point_size = get_point_size();
-    auto pointcloud_marker_ptr = std::make_shared<Marker>();
-    pointcloud_marker_ptr->header = msg->header;
-    pointcloud_marker_ptr->ns = "cluster_point_cloud";
-    pointcloud_marker_ptr->id = static_cast<int>(id);
-    pointcloud_marker_ptr->type = visualization_msgs::msg::Marker::POINTS;
-    pointcloud_marker_ptr->action = visualization_msgs::msg::Marker::ADD;
-    pointcloud_marker_ptr->pose.position.x = 0.0;
-    pointcloud_marker_ptr->pose.position.y = 0.0;
-    pointcloud_marker_ptr->pose.position.z = 0.0;
-    pointcloud_marker_ptr->pose.orientation.w = 1.0;
-    pointcloud_marker_ptr->scale.x = point_size; 
-    pointcloud_marker_ptr->scale.y = point_size;
-    pointcloud_marker_ptr->lifetime = rclcpp::Duration::from_seconds(0.15);
-    pointcloud_marker_ptr->points.clear();
-    pointcloud_marker_ptr->colors.clear();
-    pointcloud_marker_ptr->points.reserve(cluster.width * cluster.height);
-    pointcloud_marker_ptr->colors.reserve(cluster.width * cluster.height);
+    auto marker = std::make_shared<Marker>();
+    marker->header = msg->header;
+    marker->ns = "cluster_point_cloud";
+    marker->id = id++;
+    marker->type = visualization_msgs::msg::Marker::POINTS;
+    marker->action = visualization_msgs::msg::Marker::ADD;
+    marker->pose.orientation.w = 1.0;
+    marker->scale.x = marker->scale.y = get_point_size();
+    marker->lifetime = rclcpp::Duration::from_seconds(0.15);
+
+    const size_t n_points = cluster.width * cluster.height;
+    marker->points.clear();
+    marker->colors.clear();
+    marker->points.reserve(n_points);
+    marker->colors.reserve(n_points);
+
     sensor_msgs::PointCloud2ConstIterator<float> iter_x(cluster, "x");
     sensor_msgs::PointCloud2ConstIterator<float> iter_y(cluster, "y");
     sensor_msgs::PointCloud2ConstIterator<float> iter_z(cluster, "z");
     sensor_msgs::PointCloud2ConstIterator<uint8_t> iter_intensity(cluster, "intensity");
+    // Set the position of each point in the marker
     for (; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z) {
-      geometry_msgs::msg::Point point;
-      point.x = *iter_x;
-      point.y = *iter_y;
-      point.z = *iter_z;
-      pointcloud_marker_ptr->points.push_back(point);
+      geometry_msgs::msg::Point pt;
+      pt.x = *iter_x;
+      pt.y = *iter_y;
+      pt.z = *iter_z;
+      marker->points.push_back(pt);
     }
-
-    if (m_display_intensity_property.getBool()) {
-      // Use intensity to color the points
-      float intensity_threshold = m_intensity_color_scale_max.getFloat();
-      for(; iter_intensity != iter_intensity.end(); ++iter_intensity) {
-        std_msgs::msg::ColorRGBA color;
-        color.r = std::clamp(static_cast<float>(*iter_intensity) / intensity_threshold, 0.0f, 1.0f);
-        color.g = 0.0f;
-        color.b = 0.0f;
-        color.a = 1.0f;
-        pointcloud_marker_ptr->colors.push_back(color);
+    // Set the color of each point based on the selected mode
+    if (mode == INTENSITY_COLOR) {
+      for (; iter_intensity != iter_intensity.end(); ++iter_intensity) {
+        float intensity_norm = static_cast<float>(*iter_intensity) / max_intensity;
+        marker->colors.push_back(color_fn(intensity_norm));
+      }
+    } else if (mode == CLUSTER_COLOR) {
+      const auto cluster_color = generateDistinctColor(marker->id);
+      for (; iter_intensity != iter_intensity.end(); ++iter_intensity) {
+        marker->colors.push_back(cluster_color);
       }
     } else {
-      std_msgs::msg::ColorRGBA color;
-      color.r = get_point_color().redF();
-      color.g = get_point_color().greenF();
-      color.b = get_point_color().blueF();
-      color.a = 1.0f;
-      for(; iter_intensity != iter_intensity.end(); ++iter_intensity) {
-        pointcloud_marker_ptr->colors.push_back(color);
+      for (; iter_intensity != iter_intensity.end(); ++iter_intensity) {
+        marker->colors.push_back(default_color);
       }
     }
-    add_marker(pointcloud_marker_ptr);
-    id++;
+    add_marker(marker);
   }
 }
 
